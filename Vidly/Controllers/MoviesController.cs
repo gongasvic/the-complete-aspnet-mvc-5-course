c228e1fbdd9d86ce4def11fc826c48634e1c207f
Goncalo Vicente <gongas09@gmail.com>
2018-07-15 20:32:07 +0100

    test mvc course "the-complete-aspnet-mvc-5-course" udemy

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var customer = new Customer()
+            {
+                Name = "Lara"
+            };
+            var movies = new List<Movie>()
+            {
+                new Movie() {Name = "Shreck! the first"},
+                new Movie() {Name = "Shreck! the second"}
+            };
+            var CustomerMovies = new CustomerMoviesViewModel()
+            {
+                Customer = customer,
+                Movies = movies
+            };
+            return View(CustomerMovies);
+        }
+    }
+}
