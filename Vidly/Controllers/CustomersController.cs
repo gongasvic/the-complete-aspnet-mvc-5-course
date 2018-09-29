64a255bbe0c4173317db9d6f11d06681e44fce7d
Goncalo Vicente <gongas09@gmail.com>
2018-09-29 16:54:39 +0100

    More Files

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
+using Vidly.Models;
+using Vidly.ViewModels;
+using WebGrease.Css.Extensions;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+            return View(customer);
+        }
+
+        public ActionResult New()
+        {
+            var membershipTypes = _context.MembershipType.ToList();
+            var viewModel = new CustomerFormViewModel
+            {
+                MembershipTypes = membershipTypes
+            };
+            return View("CustomerForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipType.ToList()
+
+                };
+                return View("CustomerForm", viewModel);
+            }
+            if (customer.Id == 0)
+                _context.Customers.Add(customer);
+            else
+            {
+                var dbCustomer = _context.Customers.Single(c => c.Id == customer.Id);
+                dbCustomer.Name = customer.Name;
+                dbCustomer.Dob = customer.Dob;
+                dbCustomer.MembershipTypeId = customer.MembershipTypeId;
+                dbCustomer.Newsletter = customer.Newsletter;
+                dbCustomer.Email = customer.Email;
+                dbCustomer.Sex = customer.Sex;
+                dbCustomer.CountryCode = customer.CountryCode;
+                dbCustomer.PhoneNumber = customer.PhoneNumber;
+
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+            var viewModel = new CustomerFormViewModel()
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipType.ToSafeReadOnlyCollection()
+            };
+            return View("CustomerForm", viewModel);
+        }
+    }
+}
